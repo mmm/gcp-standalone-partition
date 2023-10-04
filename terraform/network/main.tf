e191982366256d4e67fb1c5b16a5c1165bb341f3
Mark Mims <markmims@google.com>
2023-10-04 09:12:52 -0600

    argh

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -16,11 +16,11 @@
 resource "google_compute_network" "tutorial" {
   name                    = var.network
   auto_create_subnetworks = false
-  project                 = var.project
+  project                 = var.project_id
 }
 
 resource "google_compute_subnetwork" "tutorial" {
-  project                  = var.project
+  project                  = var.project_id
   name                     = var.subnet
   ip_cidr_range            = var.cidr
   region                   = var.region
@@ -29,14 +29,14 @@ resource "google_compute_subnetwork" "tutorial" {
 }
 
 resource "google_compute_router" "tutorial" {
-  project = var.project
+  project = var.project_id
   name    = var.network
   network = google_compute_network.tutorial.id
   region  = var.region
 }
 
 resource "google_compute_router_nat" "tutorial" {
-  project                            = var.project
+  project                            = var.project_id
   name                               = var.network
   router                             = google_compute_router.tutorial.name
   region  = var.region
@@ -52,7 +52,7 @@ resource "google_compute_router_nat" "tutorial" {
 }
 
 resource "google_compute_firewall" "tutorial-iap-access" {
-  project = var.project
+  project = var.project_id
   name    = "${var.network}-iap-access"
   network = google_compute_network.tutorial.id
 
@@ -65,7 +65,7 @@ resource "google_compute_firewall" "tutorial-iap-access" {
 }
 
 resource "google_compute_firewall" "tutorial-allow-internal" {
-  project     = var.project
+  project     = var.project_id
   name        = "${var.network}-allow-internal"
   network     = google_compute_network.tutorial.id
   description = "Allow internal traffic on the tutorial network"
