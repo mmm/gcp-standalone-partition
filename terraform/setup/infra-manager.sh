8ec7e3b5d838ead4d6e3eaed818f7259a5ae8520
Mark Mims <markmims@google.com>
2024-01-10 09:49:00 -0700

    infra-manager is in general release now

--- terraform/setup/infra-manager.sh
+++ terraform/setup/infra-manager.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 LOCATION="us-central1"
 DEPLOYMENT_ID="setup"
@@ -8,7 +22,7 @@ GIT_REPO="https://github.com/mmm/gcp-standalone-partition"
 DIRECTORY="terraform/setup"
 INPUTS=project_id=${GOOGLE_CLOUD_PROJECT}
 
-gcloud alpha infra-manager deployments apply \
+gcloud infra-manager deployments apply \
   "projects/${GOOGLE_CLOUD_PROJECT}/locations/${LOCATION}/deployments/${DEPLOYMENT_ID}" \
   --service-account "projects/${SERVICE_ACCOUNT_PROJECT_ID}/serviceAccounts/${SERVICE_ACCOUNT_EMAIL}" \
   --git-source-repo="${GIT_REPO}" \
--- terraform/storage/infra-manager.sh
+++ terraform/storage/infra-manager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2023 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -49,7 +49,7 @@ die() {
 #   fi
 # }
 
-GCLOUD_VERSION="alpha"
+GCLOUD_VERSION="" #"alpha"
 
 get_latest_revision_of() {
   local deployment=$1
